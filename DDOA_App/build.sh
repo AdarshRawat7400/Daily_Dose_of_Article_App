7da8df7fb196658faa7e82cad239b5e8dbffe3b1
Adarsh Rawat <56146385+AdarshRawat7400@users.noreply.github.com>
2023-01-04 18:32:29 +0530

    Update build.sh

--- DDOA_App/build.sh
+++ DDOA_App/build.sh
@@ -2,11 +2,6 @@
 
 
 set -o erexit
-# Create a virtual environment
-python -m venv venv
-
-# Activate the virtual environment
-source venv/bin/activate
 
 # Install the required packages
 pip install -r requirements.txt
