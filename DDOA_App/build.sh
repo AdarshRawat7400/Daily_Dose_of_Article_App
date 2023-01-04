11aa231435a5bed3addd7c2de6d2d06243047eaf
Adarsh Rawat <56146385+AdarshRawat7400@users.noreply.github.com>
2023-01-04 17:43:09 +0530

    Create build.sh

---
+++ DDOA_App/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Python 3.9
+sudo apt-get update
+sudo apt-get install python3.9
+
+# Create a virtual environment
+python3.9 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install the required packages
+pip install -r requirements.txt
+
+# Initialize the database
+flask db init
+
+# Run database migrations
+flask db migrate
